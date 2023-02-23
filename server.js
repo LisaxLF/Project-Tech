070499214e27231c8b8a1022646d5e0f960b4f45
LisaxLF <112825800+LisaxLF@users.noreply.github.com>
2023-02-23 12:52:53 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -2,26 +2,41 @@
 const express = require('express')
 const res = require('express/lib/response')
 const app = express()
+const path = require('path')
 
 // port waar server op loopt
 const port = 3000
 
+// Set the View Engine or Template Engine
+app.set('view engine', 'ejs');
+
 // static content gebruiken
-app.use(express.static('static'))
-// een route die leidt naar home
-app.get('/', (req, res) => {
-    res.send('<img src="/images/gebouwen.jpg" width="250"><h1> Welcome to this matching-application for gamers.</h1>')
+app.use(express.static(__dirname + '/static'));
+
+// een route die leidt naar de login
+app.get('/login', (req, res) => {
+    res.send('<h1> Welcome to Gamerz, login in to continue</h1>')
 })
+// een route die leidt naar de homepage
+app.get('/', function (req, res) {
+    const gamer = {
+        gamertag: "@Geert",
+        bio: "Hoi ik ben geert welkom, ik zoek goede teammates",
+        rank: ['/static/images/icons/champ.png', '/static/images/icons/champ2.png', '/static/images/icons/champ3.png' ]
+      }
+    res.render("index", {gamer});
+  })
+
 
 // een route die leidt naar profiel
 app.get('/profiel/:user/', (req, res) => {
     console.log(`Input from ${req.params.user}`)
-    res.send('<h1>Hello ' + req.params.user + ',' + 'Dit jouw profiel</h1>')
+    res.send('<h1>Hello ' + req.params.user + ',' + 'Dit is jouw profiel</h1>')
 })
 
 
-
 // de port openen zodat de server kan lopen
-app.listen(port, () => {
-    console.log(`Turn up on port: ${port}`)
+app.listen(port, function(error){
+    if(error) throw error
+    console.log("Server created Successfully")
 })
